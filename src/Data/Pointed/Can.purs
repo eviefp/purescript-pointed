2c25d9dd0eda360b457702622716dab39b839352
Vladimir Ciobanu <vladimir-gabriel.ciobanu@ing.com>
2020-10-02 12:21:49 +0300

    Can.purs: Add module docs.

--- src/Data/Pointed/Can.purs
+++ src/Data/Pointed/Can.purs
@@ -1,4 +1,23 @@
--- | TODO: Module docs.
+-- | This module defines the type `Can`, along with instances and functions that
+-- | help use this type. Generally, 'Can' is used when the data is more vague
+-- | than simply using 'Either a b' or 'Tuple a b', i.e. when either 'a' or 'b'
+-- | might be missing or be present independently.
+-- |
+-- | 'Can' values can be compared. Note that they are only equal if they are
+-- | precisely equal.
+-- |
+-- | > (One "hello" :: Can String Int) == (Two "hello" 42 :: Can String Int)
+-- | false
+-- |
+-- | The `Semigroup` instance will never lose information and requires both
+-- | 'a' and 'b' to have 'Semigroup' instances:
+-- |
+-- | > (One "hello" :: Can String String) <> Two "world" "42"
+-- | Two "helloworld" "42"
+-- |
+-- | We can map over both arguments using `bimap`:
+-- | > bimap show (_ + 10) (Two 42 32)
+-- | Two "42" 42
 module Data.Pointed.Can where
 
 import Prelude
@@ -18,7 +37,8 @@ import Data.Traversable (class Traversable, sequenceDefault)
 import Data.Tuple (Tuple(..))
 import Data.Tuple.Nested ((/\))
 
--- | TODO: Add description.
+-- | `Can` 'a' 'b' can hold either no values, an 'a', a 'b', or both an 'a' and
+-- | a 'b'. The type is isomorphic with 'Maybe (Either (Either a b) (Tuple a b))'.
 data Can a b
   = Non
   | One a
@@ -120,6 +140,20 @@ instance traversableCan :: Traversable (Can a) where
   traverse = bitraverse pure
   sequence = sequenceDefault
 
+-- | 'Can' catamorphism (fold). Takes an input for each possible constructor and
+-- | translates it to 'c'.
+-- |
+-- | For example, we can go from some 'c :: Can a b' to a
+-- | 'Maybe (Either (Either a b) (Tuple a b))' using:
+-- |
+-- | > can Nothing (Left <<< Left) (Left <<< Right) (\a b -> Right (Tuple a b)) Non
+-- | Nothing
+-- | > can Nothing (Left <<< Left) (Left <<< Right) (\a b -> Right (Tuple a b)) (One "hello" :: Can String Int)
+-- | Just (Left (Left "hello"))
+-- | > can Nothing (Left <<< Left) (Left <<< Right) (\a b -> Right (Tuple a b)) (Eno 42 :: Can String Int)
+-- | Just (Left (Right 42))
+-- | > can Nothing (Left <<< Left) (Left <<< Right) (\a b -> Right (Tuple a b)) (Two "hello" 42 :: Can String Int)
+-- | Just (Right (Tuple "hello" 42))
 can :: forall a b c. c -> (a -> c) -> (b -> c) -> (a -> b -> c) -> Can a b -> c
 can non one eno two = case _ of
   Non     -> non
@@ -127,36 +161,148 @@ can non one eno two = case _ of
   Eno b   -> eno b
   Two a b -> two a b
 
+-- | Grabs an 'a' if it exists, otherwise returns nothing. Note that this
+-- | function grabs an 'a' from either the `One` or the `Two` constructors.
+-- |
+-- | > first (Non :: Can String Int)
+-- | Nothing
+-- | > first (One "hello" :: Can String Int)
+-- | Just "hello"
+-- | > first (Eno 42 :: Can String Int)
+-- | Nothing
+-- | > first (Two "hello" 42)
+-- | Just "hello"
 first :: forall a b. Can a b -> Maybe a
 first = can Nothing Just (const Nothing) (const <<< Just)
 
+-- | Grabs an 'b' if it exists, otherwise returns nothing. Note that this
+-- | function grabs a 'b' from either the `Eno` or the `Two` constructors.
+-- |
+-- | > second (Non :: Can String Int)
+-- | Nothing
+-- | > second (One "hello" :: Can String Int)
+-- | Nothing
+-- | > second (Eno 42 :: Can String Int)
+-- | Just 42
+-- | > second (Two "hello" 42)
+-- | Just 42
 second :: forall a b. Can a b -> Maybe b
 second = can Nothing (const Nothing) Just (const Just)
 
+-- | Grabs both an 'a' and a 'b' if they both exist, from the `Two` constructor.
+-- |
+-- | > both (Non :: Can String Int)
+-- | Nothing
+-- | > both (One "hello" :: Can String Int)
+-- | Nothing
+-- | > both (Eno 42 :: Can String Int)
+-- | Nothing
+-- | > both (Two "hello" 42)
+-- | Just (Tuple "hello" 42)
 both :: forall a b. Can a b -> Maybe (Tuple a b)
 both = can Nothing (const Nothing) (const Nothing) (\a -> Just <<< Tuple a)
 
+-- | Checks if the constructor is `Non`.
+-- |
+-- | > isNone (Non :: Can String Int)
+-- | true
+-- | > isNone (One "hello" :: Can String Int)
+-- | false
+-- | > isNone (Eno 42 :: Can String Int)
+-- | false
+-- | > isNone (Two "hello" 42)
+-- | false
 isNone :: forall a b. Can a b -> Boolean
 isNone = can true (const false) (const false) (const $ const false)
 
+-- | Checks if the constructor is `One`.
+-- |
+-- | > isOne (Non :: Can String Int)
+-- | false
+-- | > isOne (One "hello" :: Can String Int)
+-- | true
+-- | > isOne (Eno 42 :: Can String Int)
+-- | false
+-- | > isOne (Two "hello" 42)
+-- | false
 isOne :: forall a b. Can a b -> Boolean
 isOne = can false (const true) (const false) (const $ const false)
 
+-- | Checks if the constructor is `Eno`.
+-- |
+-- | > isEno (Non :: Can String Int)
+-- | false
+-- | > isEno (One "hello" :: Can String Int)
+-- | false
+-- | > isEno (Eno 42 :: Can String Int)
+-- | true
+-- | > isEno (Two "hello" 42)
+-- | false
 isEno :: forall a b. Can a b -> Boolean
 isEno = can false (const false) (const true) (const $ const false)
 
+-- | Checks if the constructor is `Two`.
+-- |
+-- | > isTwo (Non :: Can String Int)
+-- | false
+-- | > isTwo (One "hello" :: Can String Int)
+-- | false
+-- | > isTwo (Eno 42 :: Can String Int)
+-- | false
+-- | > isTwo (Two "hello" 42)
+-- | true
 isTwo :: forall a b. Can a b -> Boolean
 isTwo = can false (const false) (const false) (const $ const true)
 
+-- | Takes all 'a' values from a `Foldable` `Can`. Note that values are extracted
+-- | from both the `One` and `Two` constructors.
+-- |
+-- | > ones [Non :: Can String Int]
+-- | Nil
+-- | > ones ([One "hello", One "world"] :: Array (Can String Int))
+-- | Cons "hello" (Cons "world" Nil)
+-- | > ones ([Eno "hello", Eno "world"] :: Array (Can String Int))
+-- | Nil
+-- | > ones ([Two "hello" 42, Two "world" 1] :: Array (Can String Int))
+-- | Cons "hello" (Cons "world" Nil)
 ones :: forall a b f. Functor f => Foldable f => f (Can a b) -> List a
 ones = catMaybes <<< foldr Cons Nil <<< map first
 
+-- | Takes all 'b' values from a `Foldable` `Can`. Note that values are extracted
+-- | from both the `Eno` and `Two` constructors.
+-- |
+-- | > enos [Non :: Can String Int]
+-- | Nil
+-- | > enos ([One "hello", One "world"] :: Array (Can String Int))
+-- | Nil
+-- | > enos ([Eno "hello", Eno "world"] :: Array (Can String Int))
+-- | Cons 42 (Cons 1 Nil)
+-- | > enos ([Two "hello" 42, Two "world" 1] :: Array (Can String Int))
+-- | Cons 42 (Cons 1 Nil)
 enos :: forall a b f. Functor f => Foldable f => f (Can a b) -> List b
 enos = catMaybes <<< foldr Cons Nil <<< map second
 
+-- | Takes all 'a' and 'b' values from a `Foldable` `Can`. Note that values are
+-- | only extracted from the `Two` constructor.
+-- |
+-- | > twos [Non :: Can String Int]
+-- | Nil
+-- | > twos ([One "hello", One "world"] :: Array (Can String Int))
+-- | Nil
+-- | > twos ([Eno "hello", Eno "world"] :: Array (Can String Int))
+-- | Nil
+-- | > twos ([Two "hello" 42, Two "world" 1] :: Array (Can String Int))
+-- | Cons (Tuple "hello" 42) (Cons (Tuple "world" 1) Nil)
 twos :: forall a b f. Functor f => Foldable f => f (Can a b) -> List (Tuple a b)
 twos = catMaybes <<< foldr Cons Nil <<< map both
 
+-- | Expand a 'Can a b' to a 'Maybe a' and a 'Maybe b'. Note that the tuple
+-- | 'Tuple (Maybe a) (Maybe b)' is exactly equivalent with 'Can a b':
+-- |
+-- | - Nothing /\ Nothing ~ Non
+-- | - Just a  /\ Nothing ~ One a
+-- | - Nothing /\ Just b  ~ Eno b
+-- | - Just a  /\ Just b  ~ Two a b
 curry :: forall a b c. (Can a b -> Maybe c) -> Maybe a -> Maybe b -> Maybe c
 curry f ma mb = case ma /\ mb of
   Nothing /\ Nothing -> f Non
@@ -164,6 +310,13 @@ curry f ma mb = case ma /\ mb of
   Nothing /\ Just b  -> f (Eno b)
   Just a  /\ Just b  -> f (Two a b)
 
+-- | Contract a 'Maybe a' and a 'Maybe b' to a 'Can a b'. Note that 'Can a b'
+-- | is exactly equivalent with 'Tuple (Maybe a) (Maybe b)':
+-- |
+-- | - Non     ~ Nothing /\ Nothing
+-- | - One a   ~ Just a  /\ Nothing
+-- | - Eno b   ~ Nothing /\ Just b
+-- | - Two a b ~ Just a  /\ Just b
 uncurry :: forall a b c. (Maybe a -> Maybe b -> Maybe c) -> Can a b -> Maybe c
 uncurry f = case _ of
   Non     -> f Nothing Nothing
@@ -171,6 +324,11 @@ uncurry f = case _ of
   Eno b   -> f Nothing (Just b)
   Two a b -> f (Just a) (Just b)
 
+-- | Grab all 'a' and 'b' values from a `Foldable` 'Can a b' and combine them
+-- | each into an `Alternative` 'f' tuple.
+-- |
+-- | > partition [Non, One "hello", Eno 42, Two "world" 1] :: Tuple (List String) (List Int)
+-- | Tuple (Cons "hello" (Cons "world" Nil)) (Cons 42 (Cons 1 Nil))
 partition
   :: forall f t a b
    . Foldable t
@@ -186,6 +344,11 @@ partition = foldr go (Tuple empty empty)
       Eno b   -> Tuple fa (fb <|> pure b)
       Two a b -> Tuple (fa <|> pure a) (fb <|> pure b)
 
+-- | Maps 'Can's over a `Traversable` and partitions the values by their
+-- | position in the 'Can'.
+-- |
+-- | > mapCans (\i -> if i < 3 then One i else Eno i) [1, 2, 3, 4, 5] :: Array (Can Int Int)
+-- | Tuple [1, 2] [3, 4, 5]
 mapCans
   :: forall f t a b c
    . Alternative f
@@ -202,6 +365,16 @@ mapCans f = foldr go (Tuple empty empty)
       Eno c   -> Tuple fb (fc <|> pure c)
       Two b c -> Tuple (fb <|> pure b) (fc <|> pure c)
 
+-- | Distribute 'Can' over a 'Tuple'.
+-- |
+-- | > distribute (Non :: Can (Tuple String String) Int)
+-- | Tuple Non Non
+-- | > distribute (One (Tuple "hello" "world") :: Can (Tuple String String) Int)
+-- | Tuple (One "hello") (One "world")
+-- | > distribute (Eno 42 :: Can (Tuple String String) Int)
+-- | Tuple (Eno 42) (Eno 42)
+-- | > distribute (Two (Tuple "hello" "world") 42 :: Can (Tuple String String) Int)
+-- | Tuple (Two "hello" 42) (Two "world" 42)
 distribute :: forall a b c. Can (Tuple a b) c -> Tuple (Can a c) (Can b c)
 distribute = case _ of
   Non               -> Tuple Non Non
@@ -209,12 +382,33 @@ distribute = case _ of
   Eno c             -> Tuple (Eno c) (Eno c)
   Two (Tuple a b) c -> Tuple (Two a c) (Two b c)
 
+-- | Codistribute 'Either' over a 'Can'.
+-- |
+-- | > codistribute (Left Non :: Either (Can String Int) (Can Boolean Int))
+-- | Non
+-- | > codistribute (Left (One "hello") :: Either (Can String Int) (Can Boolean Int))
+-- | One (Left "hello")
+-- | > codistribute (Left (Eno 42) :: Either (Can String Int) (Can Boolean Int))
+-- | Eno 42
+-- | > codistribute (Left (Two "hello" 42) :: Either (Can String Int) (Can Boolean Int))
+-- | Two (Left "hello") 42
+-- | > codistribute (Right Non :: Either (Can String Int) (Can Boolean Int))
+-- | Non
+-- | > codistribute (Right (One true) :: Either (Can String Int) (Can Boolean Int))
+-- | One (Right true)
+-- | > codistribute (Right (Eno 42) :: Either (Can String Int) (Can Boolean Int))
+-- | Eno 42
+-- | > codistribute (Right (Two true 42) :: Either (Can String Int) (Can Boolean Int))
+-- | Two (Right true) 42
 codistribute :: forall a b c. Either (Can a c) (Can b c) -> Can (Either a b) c
 codistribute =
     can Non (One <<< Left ) Eno (\a c -> Two (Left a) c)
     `either`
     can Non (One <<< Right) Eno (\b c -> Two (Right b) c)
 
+-- | Reassociates a `Can` from left to right.
+-- | Note that 'Eno c' and 'Two Non c' both collapse to 'Eno (Eno c)', so
+-- | it is NOT the case that 'reassocLR <<< reassocRL = identity'.
 reassocLR :: forall a b c. Can (Can a b) c -> Can a (Can b c)
 reassocLR = case _ of
   Non -> Non
@@ -228,6 +422,9 @@ reassocLR = case _ of
   Two (Eno b) c -> Eno (Two b c)
   Two (Two a b) c -> Two a (Two b c)
 
+-- | Reassociates a `Can` from right to left.
+-- | Note that 'Eno c' and 'Two Non c' both collapse to 'Eno (Eno c)', so
+-- | it is NOT the case that 'reassocLR <<< reassocRL = identity'.
 reassocRL :: forall a b c. Can a (Can b c) -> Can (Can a b) c
 reassocRL = case _ of
   Non             -> Non
@@ -241,5 +438,15 @@ reassocRL = case _ of
   Two a (Eno c)   -> Two (One a) c
   Two a (Two b c) -> Two (Two a b) c
 
+-- | Swap the inputs for a 'Can'.
+-- |
+-- | > swap (Non :: Can String Int)
+-- | Non
+-- | > swap (One "hello" :: Can String Int)
+-- | Eno "hello"
+-- | > swap (Eno 42 :: Can String Int)
+-- | One 42
+-- | > swap (Two "hello" 42)
+-- | Two 42 "hello"
 swap :: forall a b. Can a b -> Can b a
 swap = can Non Eno One (flip Two)
