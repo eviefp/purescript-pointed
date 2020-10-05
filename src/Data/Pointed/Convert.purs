13bc70a37adbb742af95535858f30a5e045ec135
Vladimir Ciobanu <admin@cvlad.info>
2020-10-05 17:50:15 +0300

    Convert.purs: Module documentation.

--- src/Data/Pointed/Convert.purs
+++ src/Data/Pointed/Convert.purs
@@ -1,3 +1,9 @@
+-- | This module defines conversion functions between Can, Smash, and Wedge.
+-- |
+-- | The examples assume that the pointed modules are imported qualified:
+-- | > import Data.Pointed.Can as C
+-- | > import Data.Pointed.Smash as S
+-- | > import Data.Pointed.Wedge as W
 module Data.Pointed.Convert where
 
 import Prelude
@@ -9,17 +15,59 @@ import Data.Pointed.Smash as S
 import Data.Pointed.Wedge (Wedge)
 import Data.Pointed.Wedge as W
 
+-- | Convert from 'Can' to 'Wedge'.
+-- |
+-- | > canSmash (C.Non :: Can String Int)
+-- | S.Non
+-- | > canSmash (C.One "hello" :: Can String Int)
+-- | S.Non
+-- | > canSmash (C.Eno 42 :: Can String Int)
+-- | S.Non
+-- | > canSmash (C.Two "hello" 42 :: Can String Int)
+-- | S.Two "hello" 42
 canSmash :: forall a b. Can a b -> Smash a b
 canSmash = C.can S.Non (const S.Non) (const S.Non) S.Two
 
+-- | Convert from 'Wedge' to 'Can'.
+-- |
+-- | > smashCan (S.Non :: Smash String Int)
+-- | C.Non
+-- | > smashCan (S.Two "hello" 42 :: Smash String Int)
+-- | C.Two "hello" 42
 smashCan :: forall a b. Smash a b -> Can a b
 smashCan = S.smash C.Non C.Two
 
+-- | Convert from 'Can' to 'Wedge'.
+-- |
+-- | > canWedge (C.Non :: Can String Int)
+-- | W.Non
+-- | > canWedge (C.One "hello" :: Can String Int)
+-- | W.One "hello"
+-- | > canWedge (C.Eno 42 :: Can String Int)
+-- | W.Eno 42
+-- | > canWedge (C.Two "hello" 42 :: Can String Int)
+-- | W.Non
 canWedge :: forall a b. Can a b -> Wedge a b
 canWedge = C.can W.Non W.One W.Eno (\_ _ -> W.Non)
 
+-- | Convert from 'Wedge' to 'Can'.
+-- |
+-- | > wedgeCan (W.Non :: Wedge String Int)
+-- | C.Non
+-- | > wedgeCan (W.One "hello" :: Wedge String Int)
+-- | C.One "hello"
+-- | > wedgeCan (W.Eno 42 :: Wedge String Int)
+-- | C.Eno 42
 wedgeCan :: forall a b. Wedge a b -> Can a b
 wedgeCan = W.wedge C.Non C.One C.Eno
 
+-- | Convert from 'Wedge' to 'Smash', given default values for 'a' and 'b'.
+-- |
+-- | > hulkSmash "world" 1 (W.Non :: Wedge String Int)
+-- | S.Non
+-- | > hulkSmash "world" 1 (W.One "hello" :: Wedge String Int)
+-- | S.Two "hello" 1
+-- | > hulkSmash "world" 1 (W.Eno 42 :: Wedge String Int)
+-- | C.Eno "world" 42
 hulkSmash :: forall a b. a -> b -> Wedge a b -> Smash a b
 hulkSmash a b = W.wedge S.Non (flip S.Two b) (S.Two a)
