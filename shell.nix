094e1ad19dcc4769ace9b56267b1fb0d2827e14d
Vladimir Ciobanu <admin@cvlad.info>
2020-10-12 16:38:44 +0300

    Add bower to shell.nix.

--- shell.nix
+++ shell.nix
@@ -1,4 +1,15 @@
 let
   tooling = import ./default.nix;
+  pkgs = tooling.pkgs;
+  ps = tooling.purescript;
 in
-  tooling.purescript.mkShell
+  pkgs.mkShell {
+    buildInputs = [
+      ps.nodejs
+      ps.purs
+      ps.spago
+      ps.pscid
+      ps.spago2nix
+      pkgs.nodePackages.bower
+    ];
+  }
